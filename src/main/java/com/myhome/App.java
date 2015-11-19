dcfb13559500fe8a058a550bfb45e1f399ac3628
Asanga Manage <asanga.manage@auspost.com.au>
2015-11-19 16:30:50 +1100

    Initial commit

---
+++ src/main/java/com/myhome/App.java
@@ -0,0 +1,15 @@
+package com.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Asanga Manage on 19/11/2015.
+ */
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
