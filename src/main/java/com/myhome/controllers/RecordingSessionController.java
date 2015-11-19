48afbc87d859c2e9581ebb75a672300090989d15
Asanga Manage <asanga.manage@auspost.com.au>
2015-11-20 10:36:51 +1100

    Add test dependencies

--- src/main/java/com/myhome/controllers/RecordingSessionController.java
+++ src/main/java/com/myhome/controllers/RecordingSessionController.java
@@ -3,6 +3,8 @@ package com.myhome.controllers;
 import com.myhome.models.RecordingSession;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+
 /**
  * Created by Asanga on 19/11/2015.
  */
@@ -17,6 +19,13 @@ public class RecordingSessionController {
 
     @RequestMapping(value = "/{sessionId}", method = RequestMethod.DELETE)
     public RecordingSession stopSession(@PathVariable("sessionId")String sessionId) {
+        RecordingSession recordingSession = new RecordingSession(sessionId + "_SID");
+        recordingSession.setStoppped(new Date());
+        return recordingSession;
+    }
+
+    @RequestMapping(value = "/{sessionId}", method = RequestMethod.GET)
+    public RecordingSession getSession(@PathVariable("sessionId") String sessionId) {
         return new RecordingSession(sessionId + "_SID");
     }
 
--- src/main/java/com/myhome/models/RecordingSession.java
+++ src/main/java/com/myhome/models/RecordingSession.java
@@ -1,13 +1,39 @@
 package com.myhome.models;
 
+import java.util.Date;
+
 /**
  * Created by Asanga on 19/11/2015.
  */
 public class RecordingSession {
     private String sessionId;
+    private Date started;
+    private Date stoppped;
 
     public RecordingSession(String sessionId) {
         this.sessionId = sessionId;
+        this.started = new Date();
+    }
+
+    public RecordingSession(String sessionId, Date started) {
+        this.sessionId = sessionId;
+        this.started = started;
+    }
+
+    public Date getStarted() {
+        return started;
+    }
+
+    public void setStarted(Date started) {
+        this.started = started;
+    }
+
+    public Date getStoppped() {
+        return stoppped;
+    }
+
+    public void setStoppped(Date stoppped) {
+        this.stoppped = stoppped;
     }
 
     public String getSessionId() {
